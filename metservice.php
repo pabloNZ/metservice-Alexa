a7e83df654d3f88c52d261d5f8cc9bbdd24b994a
Paul Hansen <pahansen@gmail.com>
2017-01-31 16:52:13 +1300

    Updates

--- metservice.php
+++ metservice.php
@@ -6,8 +6,7 @@ $obsUrl = file_get_contents('http://www.metservice.com/publicData/localObs_lyall
 $forecastRaw = json_decode($forecastUrl);
 $obsRaw = json_decode($obsUrl);
 
-$timestamp = "20150611183741.941Z";
-$date = DateTime::createFromFormat('YmdHis.ue', $timestamp);
+$timestamp = gmdate("Y-m-d\TH:i:s\Z");
 
 $forecastData = $forecastRaw->{'days'}[0]->{'forecast'};
 $maxData = $forecastRaw->{'days'}[0]->{'max'};
@@ -22,11 +21,11 @@ $updateTime = $obsRaw->{'threeHour'}->{'dateTime'};
 $mainText = (' ' .$forecastData. ' with a high of ' .$maxData. ' degrees and low of ' .$minData. ' degrees. Right now it is ' .$tempNow. ' with ' .$windDir. ' winds of ' .$windNow. ' kilometers per hour which makes it feel like ' .$tempFeels. ' degrees');
 
 $uid = 'urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b';
-$updateDate = $date->format('Y-m-d H:i:s');
+$updateDate = $timestamp;
 $titleText = 'Metservice Weather';
 $redirectionUrl = 'http://www.metservice.co.nz';
 
-$arr = array('uid'=>$uid, 'updateDate'=>$date, 'titleText'=>$titleText, 'mainText'=>$mainText, 'redirectionUrl'=>$redirectionUrl);
+$arr = array('uid'=>$uid, 'updateDate'=>$timestamp, 'titleText'=>$titleText, 'mainText'=>$mainText, 'redirectionUrl'=>$redirectionUrl);
 
 $postResult = json_encode($arr);
 
