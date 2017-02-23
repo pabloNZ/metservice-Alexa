e90d731c0f980931617715d78e1e4c1c09571424
Paul Hansen <pabloNZ@users.noreply.github.com>
2017-02-23 16:58:18 +1300

    Region and small updates
    MickeyD expanded the code to be more versatile. I'm updating this for my own records.

--- metservice.php
+++ metservice.php
@@ -1,45 +1,83 @@
-<?php
-//Script by pabloNZ
+ <?php
+//Script by Disrespective and MickeyD @ geekzone
 
-   header('Content-Type: application/json');
+/*
+    Usage:
+        Set the feed url to http://alexa.vagabond.net.nz/metservice_weather.php?region=xxxxxx
+        Where region is as defined on the metservice website
+*/
 
-   $data = array();
+    header('Content-Type: application/json');
 
-   //format the update time as UTC like amazon expects
-   $current_time = date("U");
-   $utc_time = $current_time - date("Z");
-   $update_time = date("Y-m-d", $utc_time) . "T" . date("H:i:s", $utc_time) . ".0Z";
+    $data = array();
 
-   $data["uid"] = "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b";
-   $data["updateDate"] = $update_time;
-   $data["titleText"] = "Metservice Weather";
-   $data["mainText"] = get_forecast("lyall-bay");
-   $data["redirectionUrl"] = "http://www.metservice.com";
+    //did they specify a location?
+    $forecast_region = "lyall-bay";  //if not, default to Lyall Bay in honour of Disrespective :-)
 
-   echo json_encode($data);
+    if(isset($_GET["region"]))
+    {
+        //is it valid? First, let's sanitise the variable
+        $matches = array();
+        if(preg_match("/([a-zA-Z\-]+)/", $_GET["region"], $matches))
+        {
+            $forecast_region = strtolower($matches[1]);
+        }
+    }
 
-   function get_forecast($region)
-   {
-       //URL of targeted sites
-       $forecastUrl = file_get_contents('http://www.metservice.com/publicData/localForecast' . $region);
-       $obsUrl = file_get_contents('http://www.metservice.com/publicData/localObs_' . $region);
+    //format the update time as UTC like amazon expects
+    $current_time = date("U");
+    $utc_time = $current_time - date("Z");
+    $update_time = date("Y-m-d", $utc_time) . "T" . date("H:i:s", $utc_time) . ".0Z";
 
-       $directions = array('N' => 'northerly', 'S' => 'southerly', 'E' => 'easterly', 'W' => 'westerly', 'NE' => 'north-easterly', 'NW' => 'north-westerly', 'SE' => 'south-easterly', 'SW' => 'south-westerly');
+    $data["uid"] = "urn:uuid:1335c695-cfb8-4ebb-abbd-80da344efa6b";
+    $data["updateDate"] = $update_time;
+    $data["titleText"] = "Metservice Weather for " . $forecast_region;
+    $data["mainText"] = get_forecast($forecast_region);
+    $data["redirectionUrl"] = "http://www.metservice.com";
 
-       $forecastRaw = json_decode($forecastUrl);
-       $obsRaw = json_decode($obsUrl);
+    echo json_encode($data);
 
-       $forecastData = $forecastRaw->{'days'}[0]->{'forecast'};
-       $maxData = $forecastRaw->{'days'}[0]->{'max'};
-       $minData = $forecastRaw->{'days'}[0]->{'min'};
+    function get_forecast($region)
+    {
+        $directions = array('N' => 'northerly', 'S' => 'southerly', 'E' => 'easterly', 'W' => 'westerly', 'NE' => 'north-easterly', 'NW' => 'north-westerly', 'SE' => 'south-easterly', 'SW' => 'south-westerly');
+        $cache_url = "http://newscache.vagabond.net.nz/metservice_cache.php?region=";
 
-       $tempNow = $obsRaw->{'threeHour'}->{'temp'};
-       $windDir = isset($directions[$obsRaw->{'threeHour'}->{'windDirection'}])? $directions[$obsRaw->{'threeHour'}->{'windDirection'}] : $obsRaw->{'threeHour'}->{'windDirection'};
-       $windNow = $obsRaw->{'threeHour'}->{'windSpeed'};
-       $tempFeels = $obsRaw->{'threeHour'}->{'windChill'};
+        //URL of targeted sites
+        $forecastUrl = 'http://www.metservice.com/publicData/localForecast' . $region;
 
-       $result = ('The weather today will be ' .$forecastData. ' with a high of ' .$maxData. ' degrees and low of ' .$minData. ' degrees. Right now it is ' .$tempNow. ' with ' .$windDir. ' winds of ' .$windNow. ' kilometers per hour which makes it feel like ' .$tempFeels. ' degrees');
+        if(!UR_exists($forecastUrl))
+        {
+            $result = "I was unable to find a forecast for " . $region;
+        } else {
+            //load from the cache (if not cached, we'll get it and cache it in the future)
+            $rawJsonData = file_get_contents($cache_url . $region);
+            $forecast = json_decode($rawJsonData);
+            if(isset($forecast->{'error'}))
+            {
+                return $forecast->{'error'};
+            }
 
-       return $result;
-   }
+            $forecastData = $forecast->{'forecastFile'}->{'days'}[0]->{'forecast'};
+            $maxData = $forecast->{'forecastFile'}->{'days'}[0]->{'max'};
+            $minData = $forecast->{'forecastFile'}->{'days'}[0]->{'min'};
+
+            $tempNow = $forecast->{'obsFile'}->{'threeHour'}->{'temp'};
+            $windDir = isset($directions[$forecast->{'obsFile'}->{'threeHour'}->{'windDirection'}])? $directions[$forecast->{'obsFile'}->{'threeHour'}->{'windDirection'}] : $forecast->{'obsFile'}->{'threeHour'}->{'windDirection'};
+            $windNow = $forecast->{'obsFile'}->{'threeHour'}->{'windSpeed'};
+            $tempFeels = $forecast->{'obsFile'}->{'threeHour'}->{'windChill'};
+
+            $result = ('The weather today will be ' .$forecastData. ' with a high of ' .$maxData. ' degrees and low of ' .$minData. ' degrees. Right now it is ' .$tempNow. ' with ' .$windDir. ' winds of ' .$windNow. ' kilometers per hour which makes it feel like ' .$tempFeels. ' degrees');
+
+        }
+
+        return $result;
+    }
+
+    function UR_exists($url){
+        //function copied from Patrick at StackOverflow
+        //http://stackoverflow.com/questions/7684771/how-check-if-file-exists-from-the-url
+       $headers=get_headers($url);
+       return stripos($headers[0],"200 OK")? true:false;
+    }
 ?>
+
